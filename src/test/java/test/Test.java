60c53c073c176f2b314f72f234ef133e5af325b5
cammiermon <camille.miermon.pro@gmail.com>
2023-11-09 11:50:03 +0100

    Modif class client et main

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,5 +1,10 @@
 package test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Achat;
 import model.Adresse;
 import model.Boutique;
 import model.Client;
@@ -20,8 +25,20 @@ public class Test {
 		Jeu j4 = new Jeu("assassin's creed", c2);
 		Jeu j5 = new Jeu("fifa", c2);
 
-        Client client1 = new Client("Saquet", "Frodon");
-        Client client2 = new Client("Gamegie", "Sam");
+		Achat a1 = new Achat(j1, LocalDate.now(), 20);
+		Achat a2 = new Achat(j2, LocalDate.now(), 30); 
+		Achat a3 = new Achat(j3, LocalDate.now(), 10);  
+		
+		List<Achat> l1 = new ArrayList();
+			l1.add(a1);
+			l1.add(a3);
+			
+		List<Achat> l2 = new ArrayList();
+			l2.add(a2);
+			l2.add(a3);	
+		
+        Client client1 = new Client("Saquet", "Frodon",l1);
+        Client client2 = new Client("Gamegie", "Sam",l2);
         Adresse adresse1 = new Adresse("1", "Cul-de-Sac", "Hobbiteville");
         Boutique boutique1 = new Boutique("La maison de Bilbo", adresse1);
 	}
